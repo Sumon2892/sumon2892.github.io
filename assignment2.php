7a6382a97c311adda99ac2ee5d620c6b194461e4
Sumon2892 <65278794+Sumon2892@users.noreply.github.com>
2020-08-12 00:31:31 +0600

    My First Website

---
+++ assignment2.php
@@ -0,0 +1,22 @@
+<?php
+    $name= $_GET["name"];
+    $uname= $_GET["username"];
+    $pass= $_GET["pwd"];
+    $gender= $_GET["gender"];
+    $skill= $_GET["skill"];
+    $contact= $_GET["contact"];
+    $email= $_GET["email"];
+    $college= $_GET["opt"];
+
+    echo "Name: $name<br>";
+    echo "Username: $uname<br>";
+    echo "Password: $pass<br>";
+    echo "Gender: $gender<br>";
+    echo "Skills: $skill<br>";
+    echo "Contact: $contact<br>";
+    echo "Email: $email<br>";
+    echo "College: $college";
+
+
+
+ ?>
